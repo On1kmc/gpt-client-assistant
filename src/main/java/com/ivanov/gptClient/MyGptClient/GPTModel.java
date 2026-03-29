8d488b3faef89d5221df0988036d270cca0f74fc
On1k <On1k@yandex.ru>
2026-03-29 21:08:47 +0300

    added some models to GPTModel.java

--- src/main/java/com/ivanov/gptClient/MyGptClient/GPTModel.java
+++ src/main/java/com/ivanov/gptClient/MyGptClient/GPTModel.java
@@ -3,9 +3,15 @@ package com.ivanov.gptClient.MyGptClient;
 public enum GPTModel {
 
     GPT_5_4("gpt-5.4"),
-    GPT_5_4_MINI("gpt-5.4-mini");
+    GPT_5_4PRO("gpt-5.4-pro"),
+    GPT_5_4_MINI("gpt-5.4-mini"),
+    GPT_5_4_NANO("gpt-5.4-nano"),
+    GPT_5("gpt-5"),
+    GPT_5_MINI("gpt-5-mini"),
+    GPT_5_NANO("gpt-5-nano"),
+    GPT_4_1("gpt-4.1");
 
-    String title;
+    final String title;
 
     GPTModel(String title) {
         this.title = title;
